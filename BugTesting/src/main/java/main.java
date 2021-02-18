b324e213df3657d204f21aac327ab3106c534fee
Rajat Rajan <tug05734@temple.edu>
2021-02-18 12:39:54 -0500

    Completed sort function

--- BugTesting/src/main/java/main.java
+++ BugTesting/src/main/java/main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Rajat
@@ -25,17 +24,18 @@ public class main {
         optionsSelection();
 
     }
+
     private static void optionsSelection() {
         String[] arr = {"1. I wish to review my expenditure",
-                "2. I wish to add my expenditure",
-                "3. I wish to delete my expenditure",
-                "4. I wish to sort the expenditures",
-                "5. I wish to search for a particular expenditure",
-                "6. Close the application"
+            "2. I wish to add my expenditure",
+            "3. I wish to delete my expenditure",
+            "4. I wish to sort the expenditures",
+            "5. I wish to search for a particular expenditure",
+            "6. Close the application"
         };
-        int[] arr1 = {1,2,3,4,5,6};
-        int  slen = arr1.length;
-        for(int i=0; i<slen;i++){
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        int slen = arr1.length;
+        for (int i = 0; i < slen; i++) {
             System.out.println(arr[i]);
             // display the all the Strings mentioned in the String array
         }
@@ -49,13 +49,13 @@ public class main {
         expenses.addAll(arrlist);
         System.out.println("\nEnter your choice:\t");
         Scanner sc = new Scanner(System.in);
-        int  options =  sc.nextInt();
-        for(int j=1;j<=slen;j++){
-            if(options==j){
-                switch (options){
+        int options = sc.nextInt();
+        for (int j = 1; j <= slen; j++) {
+            if (options == j) {
+                switch (options) {
                     case 1:
                         System.out.println("Your saved expenses are listed below: \n");
-                        System.out.println(expenses+"\n");
+                        System.out.println(expenses + "\n");
                         optionsSelection();
                         break;
                     case 2:
@@ -64,16 +64,16 @@ public class main {
                         expenses.add(value);
                         System.out.println("Your value is updated\n");
                         expenses.addAll(arrlist);
-                        System.out.println(expenses+"\n");
+                        System.out.println(expenses + "\n");
                         optionsSelection();
 
                         break;
                     case 3:
                         System.out.println("You are about the delete all your expenses! \nConfirm again by selecting the same option...\n");
                         int con_choice = sc.nextInt();
-                        if(con_choice==options){
-                               expenses.clear();
-                            System.out.println(expenses+"\n");
+                        if (con_choice == options) {
+                            expenses.clear();
+                            System.out.println(expenses + "\n");
                             System.out.println("All your expenses are erased!\n");
                         } else {
                             System.out.println("Oops... try again!");
@@ -99,9 +99,11 @@ public class main {
         }
 
     }
+
     private static void closeApp() {
         System.out.println("Closing your application... \nThank you!");
-            }
+    }
+
     private static void searchExpenses(ArrayList<Integer> arrayList) {
         int leng = arrayList.size();
         System.out.println("Enter the expense you need to search:\t");
@@ -109,19 +111,31 @@ public class main {
         Scanner in = new Scanner(System.in);
         boolean found = false;
         int searchFor = in.nextInt();
-        for (int i = 0; i < arrayList.size(); i++){
-            if(arrayList.get(i).equals(searchFor)){
+        for (int i = 0; i < leng; i++) {
+            if (arrayList.get(i).equals(searchFor)) {
                 found = true;
                 break;
             }
         }
-        if(found){System.out.println("\nExpense found!\n");}
-        else{System.out.println("\nExpense not found.\n");}
+        if (found) {
+            System.out.println("\nExpense found!\n");
+        } else {
+            System.out.println("\nExpense not found.\n");
+        }
     }
+
     private static void sortExpenses(ArrayList<Integer> arrayList) {
-        int arrlength =  arrayList.size();
-       //Complete the method. The expenses should be sorted in ascending order.
+        int arrlength = arrayList.size();
+        //Complete the method. The expenses should be sorted in ascending order.
+        for(int i = 1; i < arrlength; i++){
+            int key = arrayList.get(i); 
+            int j = i - 1; 
+            while(j >= 0 && arrayList.get(j) > key){
+                arrayList.set(j + 1, arrayList.get(j));
+                j--;
+            }
+            arrayList.set(j + 1, key);
+        }
+        System.out.println(arrayList.toString());
     }
 }
-    
-
